1cfda9988d4d4d4148dea8c84ff25e837f5ea875
Christian Spielberger <christian.spielberger@gmail.com>
2018-07-05 07:07:18 +0200

    udp: Add function udp_open for socket without bind
    For (multicast) RTP transmission feature we need a function that simply creates
    an UDP socket without bind and attaching a thread for incomming data.

--- include/re_udp.h
+++ include/re_udp.h
@@ -23,6 +23,7 @@ typedef void (udp_error_h)(int err, void *arg);
 int  udp_listen(struct udp_sock **usp, const struct sa *local,
 		udp_recv_h *rh, void *arg);
 int  udp_connect(struct udp_sock *us, const struct sa *peer);
+int  udp_open(struct udp_sock **usp, int af);
 int  udp_send(struct udp_sock *us, const struct sa *dst, struct mbuf *mb);
 int  udp_send_anon(const struct sa *dst, struct mbuf *mb);
 int  udp_local_get(const struct udp_sock *us, struct sa *local);
