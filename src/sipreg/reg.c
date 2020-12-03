ba569ac2a4fe59e294599c1d2a15f9299aa126cc
Nils Jeisecke <nils.jeisecke@saltation.com>
2020-12-03 08:09:01 +0100

    sipreg: dont't force digest challenge for register
    For a re-register an expired nonce will automatically result in 401
    anyway, so no need to force that.
    
    For a de-register the dialog will be stopped after sending the REGISTER
    with expires=0 so in case of an 401 response we're out of luck anyway
    and the server must do its house-keeping.

--- src/sipreg/reg.c
+++ src/sipreg/reg.c
@@ -297,7 +297,6 @@ static int request(struct sipreg *reg, bool reset_ls)
 
 	if (reset_ls) {
 		sip_loopstate_reset(&reg->ls);
-		sip_auth_reset(reg->auth);
 	}
 
 	return sip_drequestf(&reg->req, reg->sip, true, "REGISTER", reg->dlg,
