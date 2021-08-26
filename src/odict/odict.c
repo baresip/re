b35a4cbe03d4fd4655900868d4f62ece6919ce89
Franz Auernigg <f.auernigg@commend.com>
2021-08-26 11:16:06 +0200

    odict: move odict_compare from retest to re

--- src/odict/odict.c
+++ src/odict/odict.c
@@ -10,6 +10,8 @@
 #include "re_hash.h"
 #include "re_odict.h"
 #include "odict.h"
+#include <math.h>
+#include <float.h>
 
 
 static void destructor(void *arg)
@@ -122,4 +124,95 @@ int odict_debug(struct re_printf *pf, const struct odict *o)
 	err |= re_hprintf(pf, " }");
 
 	return err;
+}
+
+
+static bool cmp_double(double a, double b)
+{
+	return fabs(a - b) < DBL_EPSILON;
+}
+
+
+bool odict_value_compare(const struct odict_entry *e1,
+	const struct odict_entry *e2, bool ignore_order)
+{
+	if (!e1 || !e2)
+		return false;
+
+	if (odict_entry_type(e1) != odict_entry_type(e2))
+		return false;
+
+	switch (odict_entry_type(e1)) {
+
+	case ODICT_OBJECT:
+		return odict_compare(odict_entry_object(e1),
+				     odict_entry_object(e2), ignore_order);
+
+	case ODICT_ARRAY:
+		return odict_compare(odict_entry_array(e1),
+				     odict_entry_array(e2), ignore_order);
+
+	case ODICT_INT:
+		if (odict_entry_int(e1) == odict_entry_int(e2))
+			return true;
+		break;
+
+	case ODICT_DOUBLE:
+		if (cmp_double(odict_entry_dbl(e1), odict_entry_dbl(e2)))
+			return true;
+		break;
+
+	case ODICT_STRING:
+		if ( 0 == str_cmp(odict_entry_str(e1), odict_entry_str(e2)))
+			return true;
+		break;
+
+	case ODICT_BOOL:
+		if (odict_entry_boolean(e1) == odict_entry_boolean(e2))
+			return true;
+		break;
+
+	case ODICT_NULL: /* no check */
+		return true;
+
+	default:
+		return false;
+	}
+
+	return false;
+}
+
+
+/* return TRUE if equal */
+bool odict_compare(const struct odict *dict1, const struct odict *dict2,
+	bool ignore_order)
+{
+	struct le *le1, *le2;
+
+	if (!dict1 || !dict2)
+		return false;
+
+	if (odict_count(dict1, true) != odict_count(dict2, true))
+		return false;
+
+	for (le1 = dict1->lst.head, le2 = dict2->lst.head;
+	     le1 && le2;
+	     le1 = le1->next, le2 = le2->next) {
+
+		const struct odict_entry *e1 = le1->data;
+		const struct odict_entry *e2;
+
+		if (ignore_order)
+			e2 = odict_lookup(dict2, odict_entry_key(e1));
+		else
+			e2 = le2->data;
+
+		if (0 != str_cmp(odict_entry_key(e1), odict_entry_key(e2)))
+			return false;
+
+		if (!odict_value_compare(e1, e2, ignore_order))
+			return false;
+	}
+
+	return true;  /* equal */
 }
