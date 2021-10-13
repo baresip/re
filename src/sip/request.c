5afe5909cb7f6902852c23f6ebba8c8c0b63399c
Christian Spielberger <c.spielberger@commend.com>
2021-10-13 08:05:12 +0200

    Revert "sip: init dst in request_next()"
    This reverts commit eaff14971b8b4e31207f219c1499a36437acbe36.

--- src/sip/request.c
+++ src/sip/request.c
@@ -214,7 +214,6 @@ static int request_next(struct sip_request *req)
 	int err;
 
  again:
-	sa_init(&dst, AF_UNSPEC);
 	rr = list_ledata(req->addrl.head);
 	if (!rr) {
 		rr = list_ledata(req->srvl.head);
