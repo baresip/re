346dc4ddddfef11837a0b2063e7d29b6a48d7f90
Sebastian Reimers <hallo@studio-link.de>
2022-01-22 10:25:11 +0100

    lock: fix win32 pthread override

--- src/lock/mod.mk
+++ src/lock/mod.mk
@@ -6,12 +6,8 @@
 
 ifdef HAVE_PTHREAD_RWLOCK
 SRCS	+= lock/rwlock.c
-else
-ifdef HAVE_PTHREAD
+else ifdef HAVE_PTHREAD
 SRCS	+= lock/lock.c
-endif
-endif
-
-ifeq ($(OS),win32)
+else ifeq ($(OS),win32)
 SRCS	+= lock/win32/lock.c
 endif
