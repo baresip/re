e605658a7a28c030d92f69f53aaf1b4b33113b25
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2022-02-23 17:54:44 +0100

    udp: fix win32 warnings

--- src/udp/udp.c
+++ src/udp/udp.c
@@ -161,7 +161,7 @@ static void udp_read(struct udp_sock *us, int fd)
 
 	src.len = sizeof(src.u);
 	n = recvfrom(fd, BUF_CAST mb->buf + us->rx_presz,
-		     mb->size - us->rx_presz, 0,
+		     SIZ_CAST mb->size - us->rx_presz, 0,
 		     &src.u.sa, &src.len);
 	if (n < 0) {
 		err = errno;
@@ -517,12 +517,14 @@ static int udp_send_internal(struct udp_sock *us, const struct sa *dst,
 
 	/* Connected socket? */
 	if (us->conn) {
-		if (send(fd, BUF_CAST mb->buf + mb->pos, mb->end - mb->pos,
+		if (send(fd, BUF_CAST mb->buf + mb->pos,
+			 SIZ_CAST mb->end - mb->pos,
 			 0) < 0)
 			return errno;
 	}
 	else {
-		if (sendto(fd, BUF_CAST mb->buf + mb->pos, mb->end - mb->pos,
+		if (sendto(fd, BUF_CAST mb->buf + mb->pos,
+			   SIZ_CAST mb->end - mb->pos,
 			   0, &dst->u.sa, dst->len) < 0)
 			return errno;
 	}
