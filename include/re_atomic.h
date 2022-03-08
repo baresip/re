4d17992c13cf86f968116ea85be77666cb57c1c3
Sebastian Reimers <hallo@studio-link.de>
2022-03-08 10:16:41 +0100

    add gcc version check (gcc >= 4.9)

--- include/re_atomic.h
+++ include/re_atomic.h
@@ -13,10 +13,17 @@ extern "C" {
 
 #ifdef HAVE_ATOMIC
 #include <stdatomic.h>
+
 #elif defined(__clang__)
 #define __CLANG_ATOMICS
+
 #elif defined(__GNUC__)
+#if __GNUC_PREREQ(4, 9)
 #define __SYNC_ATOMICS
+#else
+#error "Atomic requires gcc >= 4.9"
+#endif /* __GNUC_PREREQ */
+
 #else
 #error "Your compiler does not support atomics"
 #endif
