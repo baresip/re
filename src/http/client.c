cda7dcc2f945ca2d51c15ceb36956d3a7c87328d
Sebastian Reimers <hallo@studio-link.de>
2022-03-14 07:46:34 +0100

    http/client: close file pointer if ftell fails in read_file()

--- src/http/client.c
+++ src/http/client.c
@@ -628,8 +628,10 @@ static int read_file(char **pbuf, const char *path)
 
 	fseek(f, 0L, SEEK_END);
 	s = ftell(f);
-	if (s < 0)
+	if (s < 0) {
+		fclose(f);
 		return errno;
+	}
 	fseek(f, 0L, SEEK_SET);
 
 	buf = mem_alloc(s + 1, NULL);
