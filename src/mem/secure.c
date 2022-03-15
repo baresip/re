4f258fa54329aee857ad18cb11a6cddcfa43d967
Sebastian Reimers <hallo@studio-link.de>
2022-03-15 12:05:03 +0100

    mem/mem_seccmp: fix volatile usage
    There is no well-defined meaning to the use of volatile to qualify a function parameter.
    Fixes:
    https://sonarcloud.io/project/issues?issues=AX9vBNPdYH-a2i9oun-G&open=AX9vBNPdYH-a2i9oun-G&id=baresip_re

--- src/mem/secure.c
+++ src/mem/secure.c
@@ -21,17 +21,17 @@
  *         0 if both byte strings matching
  *         a positive number if not matching
  */
-int mem_seccmp(const volatile uint8_t *volatile s1,
-	       const volatile uint8_t *volatile s2,
-	       size_t n)
+int mem_seccmp(const uint8_t *s1, const uint8_t *s2, size_t n)
 {
 	uint8_t val = 0;
+	const volatile uint8_t *p1 = s1;
+	const volatile uint8_t *p2 = s2;
 
-	if (!s1 || !s2)
+	if (!p1 || !p2)
 		return -1;
 
 	while (n--)
-		val |= *s1++ ^ *s2++;
+		val |= *p1++ ^ *p2++;
 
 	return val;
 }
