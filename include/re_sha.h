518f3ee06291d71cc04ecf6848125eb87e024b0b
Sebastian Reimers <hallo@studio-link.de>
2022-03-28 10:48:42 +0200

    sha: add sha 256 and 512 digest length OpenSSL compats

--- include/re_sha.h
+++ include/re_sha.h
@@ -6,10 +6,22 @@
 
 /** SHA-1 Digest size in bytes */
 #define SHA1_DIGEST_SIZE 20
+#define SHA256_DIGEST_SIZE 32
+#define SHA512_DIGEST_SIZE 64
 
 #ifndef SHA_DIGEST_LENGTH
 /** SHA-1 Digest size in bytes (OpenSSL compat) */
 #define SHA_DIGEST_LENGTH SHA1_DIGEST_SIZE
+#endif
+
+#ifndef SHA256_DIGEST_LENGTH
+/** SHA-256 Digest size in bytes (OpenSSL compat) */
+#define SHA256_DIGEST_LENGTH SHA256_DIGEST_SIZE
+#endif
+
+#ifndef SHA512_DIGEST_LENGTH
+/** SHA-512 Digest size in bytes (OpenSSL compat) */
+#define SHA512_DIGEST_LENGTH SHA512_DIGEST_SIZE
 #endif
 
 void sha1(const uint8_t *d, size_t n, uint8_t *md);
