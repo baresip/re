b7da5c94959c6bc9d7b719ac64e83d1685237bd4
Sebastian Reimers <hallo@studio-link.de>
2022-05-11 16:05:40 +0200

    fix cmake and make

--- src/thread/mod.mk
+++ src/thread/mod.mk
@@ -1,9 +1,6 @@
-ifdef HAVE_THREADS
-SRCS	+= thread/thread.c
-else ifdef HAVE_PTHREAD
-SRCS	+= thread/thread.c
-SRCS	+= thread/posix.c
-else ifeq ($(OS),win32)
-SRCS	+= thread/thread.c
+ifeq ($(OS),win32)
 SRCS	+= thread/win32.c
+else
+SRCS	+= thread/posix.c
 endif
+SRCS	+= thread/thread.c
