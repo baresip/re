2fb5cf3d117091b2899663b215efd06cc25f7dea
Maximilian Fridrich <m.fridrich@commend.com>
2022-07-14 13:39:33 +0200

    sipsess: combine reinvite and update handlers

--- src/sipsess/listen.c
+++ src/sipsess/listen.c
@@ -224,10 +224,11 @@ static void prack_handler(struct sipsess_sock *sock, const struct sip_msg *msg)
 }
 
 
-static void reinvite_handler(struct sipsess_sock *sock,
+static void target_refresh_handler(struct sipsess_sock *sock,
 			     const struct sip_msg *msg)
 {
 	struct sip *sip = sock->sip;
+	bool is_invite = true;
 	struct sipsess *sess;
 	struct mbuf *desc;
 	char m[256];
@@ -239,12 +240,13 @@ static void reinvite_handler(struct sipsess_sock *sock,
 		return;
 	}
 
+	is_invite = !pl_strcmp(&msg->met, "INVITE");
 	if (!sip_dialog_rseq_valid(sess->dlg, msg)) {
 		(void)sip_treply(NULL, sip, msg, 500, "Server Internal Error");
 		return;
 	}
 
-	if (sess->st || sess->awaiting_answer) {
+	if ((is_invite && sess->st) || sess->awaiting_answer) {
 		(void)sip_treplyf(NULL, NULL, sip, msg, false,
 				  500, "Server Internal Error",
 				  "Retry-After: 5\r\n"
@@ -253,7 +255,7 @@ static void reinvite_handler(struct sipsess_sock *sock,
 		return;
 	}
 
-	if (sess->req) {
+	if (is_invite && sess->req) {
 		(void)sip_treply(NULL, sip, msg, 491, "Request Pending");
 		return;
 	}
@@ -277,53 +279,6 @@ static void reinvite_handler(struct sipsess_sock *sock,
 }
 
 
-static void update_handler(struct sipsess_sock *sock,
-			   const struct sip_msg *msg)
-{
-	struct sip *sip = sock->sip;
-	struct sipsess *sess;
-	struct mbuf *desc;
-	char m[256];
-	int err;
-
-	sess = sipsess_find(sock, msg);
-	if (!sess || sess->terminated) {
-		(void)sip_treply(NULL, sip, msg, 481, "Call Does Not Exist");
-		return;
-	}
-
-	if (!sip_dialog_rseq_valid(sess->dlg, msg)) {
-		(void)sip_treply(NULL, sip, msg, 500, "Server Internal Error");
-		return;
-	}
-
-	if (sess->awaiting_answer) {
-		(void)sip_treplyf(NULL, NULL, sip, msg, false,
-				  500, "Server Internal Error",
-				  "Retry-After: 5\r\n"
-				  "Content-Length: 0\r\n"
-				  "\r\n");
-		return;
-	}
-
-	err = sess->offerh(&desc, msg, sess->arg);
-	if (err) {
-		(void)sip_reply(sip, msg, 488, str_error(err, m, sizeof(m)));
-		return;
-	}
-
-	(void)sip_dialog_update(sess->dlg, msg);
-	(void)sipsess_reply_2xx(sess, msg, 200, "OK", desc, NULL, NULL);
-
-	/* pending modifications considered outdated;
-	 * sdp may have changed in above exchange */
-	sess->desc = mem_deref(sess->desc);
-	sess->modify_pending = false;
-	tmr_cancel(&sess->tmr);
-	mem_deref(desc);
-}
-
-
 static void invite_handler(struct sipsess_sock *sock,
 			   const struct sip_msg *msg)
 {
@@ -338,14 +293,14 @@ static bool request_handler(const struct sip_msg *msg, void *arg)
 	if (!pl_strcmp(&msg->met, "INVITE")) {
 
 		if (pl_isset(&msg->to.tag))
-			reinvite_handler(sock, msg);
+			target_refresh_handler(sock, msg);
 		else
 			invite_handler(sock, msg);
 
 		return true;
 	}
 	else if (!pl_strcmp(&msg->met, "UPDATE")) {
-		update_handler(sock, msg);
+		target_refresh_handler(sock, msg);
 		return true;
 	}
 	else if (!pl_strcmp(&msg->met, "ACK")) {
