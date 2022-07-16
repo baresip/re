59f94eeef257831d079bf993d1cdd42a5974684b
Sebastian Reimers <hallo@studio-link.de>
2022-07-16 08:50:31 +0200

    types,mk: remove HAVE_STDBOOL_H (#439)
    Since C99 all compilers should have support for stdbool.h

--- include/re_types.h
+++ include/re_types.h
@@ -15,32 +15,11 @@ typedef SSIZE_T ssize_t;
 #endif
 
 /*
- * Basic integral types from C99
+ * Basic integral types and boolean from C99
  */
-
 #include <inttypes.h>
+#include <stdbool.h>
 
-
-/*
- * Boolean type
- * see http://www.opengroup.org/onlinepubs/000095399/basedefs/stdbool.h.html
- *     www.gnu.org/software/autoconf/manual/html_node/Particular-Headers.html
- */
-#ifdef HAVE_STDBOOL_H
-# include <stdbool.h>
-#else
-# ifndef HAVE__BOOL
-#  ifdef __cplusplus
-typedef bool _Bool;
-#  else
-#   define _Bool signed char
-#  endif
-# endif
-# define bool _Bool
-# define false 0
-# define true 1
-# define __bool_true_false_are_defined 1
-#endif
 
 /* Needed for MS compiler */
 #ifdef _MSC_VER
