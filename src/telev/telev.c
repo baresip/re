8ee6930c1e999bcf9f485621f4e964991523e4c6
Christian Spielberger <c.spielberger@commend.com>
2022-09-20 11:47:05 +0200

    telev: make parameter const and add NULL pointer check

--- src/telev/telev.c
+++ src/telev/telev.c
@@ -341,8 +341,11 @@ int telev_poll(struct telev *tel, bool *marker, struct mbuf *mb)
 }
 
 
-bool telev_is_empty(struct telev *tel)
+bool telev_is_empty(const struct telev *tel)
 {
+	if (!tel)
+		return true;
+
 	return tel->state == IDLE && !mbuf_get_left(tel->mb);
 }
 
