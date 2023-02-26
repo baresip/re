0a44e9bbcfbf5cbf2110e3d2c38fecd7b7847dcd
Sebastian Reimers <hallo@studio-link.de>
2023-02-26 10:50:01 +0100

    async: fix cancel memory leaks (#705)
    * async: fix cancel memory leaks
    
    * reset id too

--- src/async/async.c
+++ src/async/async.c
@@ -270,29 +270,36 @@ void re_async_cancel(struct re_async *async, intptr_t id)
 
 	mtx_lock(&async->mtx);
 
-	LIST_FOREACH(&async->workl, le)
-	{
+	le = list_head(&async->workl);
+	while (le) {
 		struct async_work *w = le->data;
+
+		le = le->next;
 
 		if (w->id != id)
 			continue;
 
+		w->id	 = 0;
 		w->workh = NULL;
 		w->cb	 = NULL;
 		w->arg	 = mem_deref(w->arg);
 		list_move(&w->le, &async->freel);
 	}
 
-	LIST_FOREACH(&async->curl, le)
-	{
+	le = list_head(&async->curl);
+	while (le) {
 		struct async_work *w = le->data;
+
+		le = le->next;
 
 		if (w->id != id)
 			continue;
 
+		w->id	 = 0;
 		w->workh = NULL;
 		w->cb	 = NULL;
 		w->arg	 = mem_deref(w->arg);
+		list_move(&w->le, &async->freel);
 	}
 
 	mtx_unlock(&async->mtx);
