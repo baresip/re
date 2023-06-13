5b383230802eb0108a13d9c126b8405fc93fb2db
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2023-06-13 08:44:37 +0200

    rtpext: add doxygen comments (#846)

--- include/re_rtpext.h
+++ include/re_rtpext.h
@@ -10,7 +10,7 @@
  */
 
 #define RTPEXT_HDR_SIZE        4
-#define RTPEXT_TYPE_MAGIC 0xbede
+#define RTPEXT_TYPE_MAGIC 0xbede  /* One-Byte header */
 
 enum {
 	RTPEXT_ID_MIN  =  1,
@@ -22,10 +22,12 @@ enum {
 	RTPEXT_LEN_MAX = 16,
 };
 
+
+/** Defines an RTP header extension */
 struct rtpext {
-	unsigned id:4;
-	unsigned len:4;
-	uint8_t data[RTPEXT_LEN_MAX];
+	unsigned id:4;                /**< Identifier             */
+	unsigned len:4;               /**< Length of data [bytes] */
+	uint8_t data[RTPEXT_LEN_MAX]; /**< Data field             */
 };
 
 
