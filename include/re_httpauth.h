bc549f5f946a18054be08c1aacef3bacf1cacb6a
Christoph Huber <c.huber@commend.com>
2023-07-13 17:28:13 +0200

    httpauth: basic challenge creation and verification functions (#875)
    * httpauth: new httpauth_basic challenge creation and verification
    
    * httpauth: httpauth_basic challenge test case
    
    * add missing doxygen parameter hval
    
    * httpauth: simplify printer function
    
    * httpauth: use mem_seccmp and clean memory afterwards

--- include/re_httpauth.h
+++ include/re_httpauth.h
@@ -41,6 +41,13 @@ struct httpauth_basic {
 	struct pl auth;
 };
 
+struct httpauth_basic_req {
+	char *realm;
+
+	/* optional */
+	char *charset;
+};
+
 
 int httpauth_digest_challenge_decode(struct httpauth_digest_chall *chall,
 				     const struct pl *hval);
@@ -61,3 +68,11 @@ int httpauth_basic_decode(struct httpauth_basic *basic,
 int httpauth_basic_make_response(struct httpauth_basic *basic,
 		const char *user, const char *pwd);
 int httpauth_basic_encode(const struct httpauth_basic *basic, struct mbuf *mb);
+
+
+int httpauth_basic_request_print(struct re_printf *pf,
+	const struct httpauth_basic_req *req);
+int httpauth_basic_verify(const struct pl *hval, const char *user,
+	const char *passwd);
+int httpauth_basic_request(struct httpauth_basic_req **preq,
+	const char *realm, const char *charset);
--- src/httpauth/basic.c
+++ src/httpauth/basic.c
@@ -123,3 +123,127 @@ int httpauth_basic_encode(const struct httpauth_basic *basic, struct mbuf *mb)
 	mbuf_set_pos(mb, 0);
 	return 0;
 }
+
+/* HTTPAUTH BASIC REQUESTS*/
+
+static void httpauth_basic_request_destructor(void *arg)
+{
+	struct httpauth_basic_req *req = arg;
+
+	mem_deref(req->realm);
+	mem_deref(req->charset);
+}
+
+
+int httpauth_basic_request_print(struct re_printf *pf,
+	const struct httpauth_basic_req *req)
+{
+	int err = 0;
+
+	if (!pf || !req)
+		return EINVAL;
+
+	err = re_hprintf(pf, "Basic realm=\"%s\"", req->realm);
+	if (str_isset(req->charset))
+		err |= re_hprintf(pf, ", charset=\"%s\"", req->charset);
+
+	return err;
+}
+
+
+/**
+ * Verify received credentials
+ *
+ * @param hval   http authentication header value containing the credentials
+ * @param user   user name (may be an UTF-8 string)
+ * @param passwd user password (may be an UTF-8 string)
+ *
+ * @return 0 if successfully verified, otherwise errorcode
+ */
+int httpauth_basic_verify(const struct pl *hval, const char *user,
+	const char *passwd)
+{
+	struct pl b64c = PL_INIT;
+	struct mbuf *mb = NULL;
+	char *c = NULL;
+	size_t clen = 0;
+	int err = 0;
+
+	if (!hval || !user || !passwd)
+		return EINVAL;
+
+	mb = mbuf_alloc(str_len(user) + str_len(passwd) + 1);
+	if (!mb)
+		return ENOMEM;
+
+	if (re_regex(hval->p, hval->l, "[ \t\r\n]*Basic[ \t\r\n]+[~ \t\r\n]*",
+		NULL, NULL, &b64c) || !pl_isset(&b64c)) {
+		err = EBADMSG;
+		goto out;
+	}
+
+	clen = b64c.l;
+	c = mem_zalloc(clen, NULL);
+	if (!c) {
+		err = ENOMEM;
+		goto out;
+	}
+
+	err = base64_decode(b64c.p, b64c.l, (uint8_t *) c, &clen);
+	if (err)
+		goto out;
+
+	err = mbuf_printf(mb, "%s:%s", user, passwd);
+	if (err)
+		goto out;
+
+	if (mem_seccmp(mb->buf, (uint8_t *)c, clen) != 0)
+		err = EACCES;
+
+out:
+	if (c)
+		mem_secclean(c, clen);
+
+	if (mb)
+		mem_secclean(mb->buf, mb->size);
+
+	mem_deref(c);
+	mem_deref(mb);
+
+	return err;
+}
+
+
+/**
+ * Create a Basic Authentication Request
+ *
+ * @param preq    httpauth_basic_req object ptr
+ * @param realm   realm
+ * @param charset optional charset
+ *
+ * @return 0 if successful, otherwise errorcode
+ */
+int httpauth_basic_request(struct httpauth_basic_req **preq,
+	const char *realm, const char *charset)
+{
+	struct httpauth_basic_req *req = NULL;
+	int err = 0;
+
+	if (!preq || !realm)
+		return EINVAL;
+
+	req = mem_zalloc(sizeof(*req), httpauth_basic_request_destructor);
+	if (!req)
+		return ENOMEM;
+
+	err = str_dup(&req->realm, realm);
+	if (str_isset(charset) && str_casecmp(charset, "UTF-8") == 0)
+		err |= str_dup(&req->charset, charset);
+
+	if (err)
+		mem_deref(req);
+	else
+		*preq = req;
+
+	return err;
+}
