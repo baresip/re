b05e7164e0a302155ffccd68bec1839c805432e5
Sebastian Reimers <hallo@studio-link.de>
2023-10-10 21:22:30 +0200

    trace: add id handling
    Add identifier handling for custom id tracking like:
    
    {"cat":"jbuf","pid":490156,"tid":490156,"ts":1415394,"ph":"I","name":"flush","id":"call1_video"}

--- include/re_trace.h
+++ include/re_trace.h
@@ -3,6 +3,8 @@
  * JSON traces (chrome://tracing)
  */
 
+struct pl;
+
 typedef enum {
 	RE_TRACE_ARG_NONE,
 	RE_TRACE_ARG_INT,
@@ -14,37 +16,46 @@ typedef enum {
 int re_trace_init(const char *json_file);
 int re_trace_close(void);
 int re_trace_flush(void);
-void re_trace_event(const char *cat, const char *name, char ph, void *id,
-			int32_t async_id, re_trace_arg_type arg_type,
-			const char *arg_name, void *arg_value);
+void re_trace_event(const char *cat, const char *name, char ph, struct pl *id,
+		    re_trace_arg_type arg_type, const char *arg_name,
+		    void *arg_value);
 
 #ifdef RE_TRACE_ENABLED
 
 #define RE_TRACE_BEGIN(c, n) \
-	re_trace_event(c, n, 'B', 0, 0, RE_TRACE_ARG_NONE, NULL, NULL)
+	re_trace_event(c, n, 'B', NULL, RE_TRACE_ARG_NONE, NULL, NULL)
 #define RE_TRACE_END(c, n) \
-	re_trace_event(c, n, 'E', 0, 0, RE_TRACE_ARG_NONE, NULL, NULL)
+	re_trace_event(c, n, 'E', NULL, RE_TRACE_ARG_NONE, NULL, NULL)
 
 #define RE_TRACE_ID_BEGIN(c, n, id) \
-	re_trace_event(c, n, 'B', 0, id, RE_TRACE_ARG_NONE, NULL, NULL)
+	re_trace_event(c, n, 'B', id, RE_TRACE_ARG_NONE, NULL, NULL)
 #define RE_TRACE_ID_END(c, n, id) \
-	re_trace_event(c, n, 'E', 0, id, RE_TRACE_ARG_NONE, NULL, NULL)
+	re_trace_event(c, n, 'E', id, RE_TRACE_ARG_NONE, NULL, NULL)
 
 #define RE_TRACE_INSTANT(c, n) \
-	re_trace_event(c, n, 'I', 0, 0, RE_TRACE_ARG_NONE, NULL, NULL)
+	re_trace_event(c, n, 'I', NULL, RE_TRACE_ARG_NONE, NULL, NULL)
 #define RE_TRACE_INSTANT_C(c, n, vname, str) \
-	re_trace_event(c, n, 'I', 0, 0, RE_TRACE_ARG_STRING_CONST, \
+	re_trace_event(c, n, 'I', NULL, RE_TRACE_ARG_STRING_CONST, \
 	vname, (void *)(str))
 #define RE_TRACE_INSTANT_I(c, n, i) \
-	re_trace_event(c, n, 'I', 0, 0, RE_TRACE_ARG_INT, \
+	re_trace_event(c, n, 'I', NULL, RE_TRACE_ARG_INT, \
+	n, (void *)(intptr_t)i)
+
+#define RE_TRACE_ID_INSTANT(c, n, id) \
+	re_trace_event(c, n, 'I', id, RE_TRACE_ARG_NONE, NULL, NULL)
+#define RE_TRACE_ID_INSTANT_C(c, n, vname, str, id) \
+	re_trace_event(c, n, 'I', id, RE_TRACE_ARG_STRING_CONST, \
+	vname, (void *)(str))
+#define RE_TRACE_ID_INSTANT_I(c, n, i, id) \
+	re_trace_event(c, n, 'I', id, RE_TRACE_ARG_INT, \
 	n, (void *)(intptr_t)i)
 
 #define RE_TRACE_PROCESS_NAME(n) \
-	re_trace_event("", "process_name", 'M', 0, 0, \
+	re_trace_event("", "process_name", 'M', NULL, \
 	RE_TRACE_ARG_STRING_COPY, \
 	"name", (void *)(n))
 #define RE_TRACE_THREAD_NAME(n) \
-	re_trace_event("", "thread_name", 'M', 0, 0, \
+	re_trace_event("", "thread_name", 'M', NULL, \
 	RE_TRACE_ARG_STRING_COPY, \
 	"name", (void *)(n))
 
@@ -57,6 +68,9 @@ void re_trace_event(const char *cat, const char *name, char ph, void *id,
 #define RE_TRACE_INSTANT(c, n)
 #define RE_TRACE_INSTANT_C(c, n, str)
 #define RE_TRACE_INSTANT_I(c, n, i)
+#define RE_TRACE_ID_INSTANT(c, n, id)
+#define RE_TRACE_ID_INSTANT_C(c, n, str, id)
+#define RE_TRACE_ID_INSTANT_I(c, n, i, id)
 #define RE_TRACE_PROCESS_NAME(n)
 #define RE_TRACE_THREAD_NAME(n)
 
