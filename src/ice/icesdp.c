57912480898f1a08bd59652110e752e87a7d1546
Landry Breuil <landryb@users.noreply.github.com>
2023-10-23 09:16:51 +0200

    ice: AI_V4MAPPED doesn't exist on OpenBSD (#989)

--- src/ice/icesdp.c
+++ src/ice/icesdp.c
@@ -209,7 +209,11 @@ static int getaddr_rcand(void *arg)
 {
 	struct rcand *rcand = arg;
 	struct addrinfo *res, *res0 = NULL;
+#ifndef OPENBSD
 	struct addrinfo hints = {.ai_flags  = AI_V4MAPPED | AI_ADDRCONFIG,
+#else
+	struct addrinfo hints = {.ai_flags  = AI_ADDRCONFIG,
+#endif
 				 .ai_family = rcand->ai_family};
 	int err;
 
