1fe98572204183beeda5e577fc13a1190e6d5f45
Lars Immisch <lars@ibp.de>
2023-11-21 10:25:31 +0100

    Use TEST_ERR correctly, cut beep.wav to exactly 67 ms.

--- test/aupos.c
+++ test/aupos.c
@@ -24,18 +24,15 @@ int test_auposition(void)
 	re_snprintf(path, sizeof(path), "%s/beep.wav", test_datapath());
 
 	int err = aufile_open(&af, &prm, path, AUFILE_READ);
-	if (err)
-		TEST_ERR(err);
+	TEST_ERR(err);
 
 	err = aufile_set_position(af, &prm, 67);
-	if (err)
-		TEST_ERR(err);
+	TEST_ERR(err);
 
 	/* That file is 67 ms long, so we shouldn't read anything */
 	size_t size = sizeof(buffer);
 	err = aufile_read(af, buffer, &size);
-	if (err)
-		TEST_ERR(err);
+	TEST_ERR(err);
 
 	TEST_EQUALS(0, size);
 
