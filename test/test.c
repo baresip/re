5c12be21d524001f82f07d0232a0960aac339690
Alfred E. Heggestad <114750+alfredh@users.noreply.github.com>
2024-06-06 17:32:21 +0200

    test: add test_rtcp_loop() (#1137)

--- test/test.c
+++ test/test.c
@@ -162,6 +162,7 @@ static const struct test tests[] = {
 	TEST(test_rtcp_decode_badmsg),
 	TEST(test_rtcp_packetloss),
 	TEST(test_rtcp_twcc),
+	TEST(test_rtcp_loop),
 	TEST(test_sa_class),
 	TEST(test_sa_cmp),
 	TEST(test_sa_decode),
