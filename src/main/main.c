f4158731ed4ffa951e2d8c4f07776beea5f1d20a
jobo <38849089+jobo-zt@users.noreply.github.com>
2024-10-17 20:19:25 +0800

    main: Fix the array out of bounds error of variable

--- src/main/main.c
+++ src/main/main.c
@@ -757,11 +757,10 @@ static int fd_poll(struct re *re)
 
 		for (i = 0; (i < re->maxfds) && (cfds < nfds); i++) {
 			fhs = re->fhsl[i];
-
+			
+			++cfds;
 			if (!fhs || !fhs->fh)
 				continue;
-
-			++cfds;
 
 			re_sock_t fd = fhs->fd;
 			if (fhs->flags & FD_READ)
