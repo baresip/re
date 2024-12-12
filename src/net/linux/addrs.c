3ac7ded41f089862ccc8eecc87a6f071f15056a5
Sebastian Reimers <hallo@studio-link.de>
2024-12-12 13:54:36 +0100

    move macros

--- src/net/linux/addrs.c
+++ src/net/linux/addrs.c
@@ -8,25 +8,11 @@
 #include <re_fmt.h>
 #include <re_sa.h>
 #include <re_net.h>
+#include "macros.h"
 
 #define DEBUG_MODULE "linuxaddrs"
 #define DEBUG_LEVEL 5
 #include <re_dbg.h>
-
-/* Override macros to avoid casting alignment warning */
-#undef RTM_RTA
-#define RTM_RTA(r) (void *)(((char *)(r)) + NLMSG_ALIGN(sizeof(struct rtmsg)))
-#undef RTA_NEXT
-#define RTA_NEXT(rta, len)                                                    \
-	((len) -= RTA_ALIGN((rta)->rta_len),                                  \
-	 (void *)(((char *)(rta)) + RTA_ALIGN((rta)->rta_len)))
-#undef NLMSG_NEXT
-#define NLMSG_NEXT(nlh, len)                                                  \
-	((len) -= NLMSG_ALIGN((nlh)->nlmsg_len),                              \
-	 (void *)(((char *)(nlh)) + NLMSG_ALIGN((nlh)->nlmsg_len)))
-#undef IFA_RTA
-#define IFA_RTA(r)                                                            \
-	((void *)(((char *)(r)) + NLMSG_ALIGN(sizeof(struct ifaddrmsg))))
 
 
 static void parse_rtattr(struct rtattr *tb[], struct rtattr *rta, int len)
