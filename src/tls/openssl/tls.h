b87f4a42a4c247f596b9e30528cf20c4e1626b51
Sebastian Reimers <hallo@studio-link.de>
2025-05-12 15:48:06 +0200

    cleanup

--- src/tls/openssl/tls.h
+++ src/tls/openssl/tls.h
@@ -13,13 +13,11 @@
  * Mapping of feature macros
  */
 
-#if !defined(LIBRESSL_VERSION_NUMBER)
-#define SSL_state SSL_get_state
-#define SSL_ST_OK TLS_ST_OK
-#endif
-
 #if defined(LIBRESSL_VERSION_NUMBER)
 typedef int (*SSL_verify_cb)(int preverify_ok, X509_STORE_CTX *x509_ctx);
+#else
+#define SSL_state SSL_get_state
+#define SSL_ST_OK TLS_ST_OK
 #endif
 
 typedef X509_NAME*(tls_get_certfield_h)(const X509 *);
