b340d79b8c209c8bde8c49b7b9f12eab9d7a2d0a
Alfred E. Heggestad <114750+alfredh@users.noreply.github.com>
2025-08-29 11:21:57 +0200

    test: add support for TURN mock-server authentication

--- test/mock/turnsrv.c
+++ test/mock/turnsrv.c
@@ -15,6 +15,16 @@
 
 enum {
 	TCP_MAX_LENGTH = 2048,
+
+	NONCE_EXPIRY   = 3600,  /* seconds */
+	NONCE_MAX_SIZE = 48,
+	NONCE_MIN_SIZE = 33,
+};
+
+
+struct auth_context {
+	uint8_t key[MD5_SIZE];
+	size_t keylen;
 };
 
 
@@ -126,9 +136,160 @@ static void relay_udp_recv(const struct sa *src, struct mbuf *mb, void *arg)
 }
 
 
+static const char *mknonce(struct turnserver *turn, char *nonce,
+			   time_t now, const struct sa *src)
+{
+	uint8_t key[MD5_SIZE];
+	uint64_t nv[3];
+
+	nv[0] = now;
+	nv[1] = turn->auth_secret;
+	nv[2] = sa_hash(src, SA_ADDR);
+
+	md5((uint8_t *)nv, sizeof(nv), key);
+
+	(void)re_snprintf(nonce, NONCE_MAX_SIZE + 1, "%w%llx",
+			  key, sizeof(key), nv[0]);
+
+	return nonce;
+}
+
+
+static bool nonce_validate(struct turnserver *turn,
+			   const char *nonce, time_t now, const struct sa *src)
+{
+	uint8_t nkey[MD5_SIZE], ckey[MD5_SIZE];
+	uint64_t nv[3];
+	struct pl pl;
+
+	pl.p = nonce;
+	pl.l = str_len(nonce);
+
+	if (pl.l < NONCE_MIN_SIZE || pl.l > NONCE_MAX_SIZE) {
+		DEBUG_NOTICE("auth: bad nonce length (%zu)\n", pl.l);
+		return false;
+	}
+
+	for (size_t i=0; i<sizeof(nkey); i++) {
+		nkey[i]  = ch_hex(*pl.p++) << 4;
+		nkey[i] += ch_hex(*pl.p++);
+		pl.l -= 2;
+	}
+
+	nv[0] = pl_x64(&pl);
+	nv[1] = turn->auth_secret;
+	nv[2] = sa_hash(src, SA_ADDR);
+
+	md5((uint8_t *)nv, sizeof(nv), ckey);
+
+	if (memcmp(nkey, ckey, MD5_SIZE)) {
+		DEBUG_NOTICE("auth: invalid nonce (%j)\n", src);
+		return false;
+	}
+
+	int64_t age = now - nv[0];
+
+	if (age < 0 || age > NONCE_EXPIRY) {
+		DEBUG_NOTICE("auth: nonce expired, age: %lli secs\n", age);
+		return false;
+	}
+
+	return true;
+}
+
+
+static int auth_get_ha1(struct turnserver *turn, const char *username,
+			uint8_t *ha1)
+{
+	static const char auth_password[] = "password";
+
+	return md5_printf(ha1, "%s:%s:%s",
+			  username, turn->auth_realm, auth_password);
+}
+
+
+static bool auth_request(struct turnserver *turn,
+			 struct auth_context *ctx, int proto, void *sock,
+			 const struct sa *src, const struct stun_msg *msg)
+{
+	struct stun_attr *mi, *user, *realm, *nonce;
+	const time_t now = time(NULL);
+	char nstr[NONCE_MAX_SIZE + 1];
+	int err;
+
+	mi    = stun_msg_attr(msg, STUN_ATTR_MSG_INTEGRITY);
+	user  = stun_msg_attr(msg, STUN_ATTR_USERNAME);
+	realm = stun_msg_attr(msg, STUN_ATTR_REALM);
+	nonce = stun_msg_attr(msg, STUN_ATTR_NONCE);
+
+	if (!mi) {
+		err = stun_ereply(proto, sock, src, 0, msg,
+				  401, "Unauthorized",
+				  NULL, 0, false, 2,
+				  STUN_ATTR_REALM, turn->auth_realm,
+				  STUN_ATTR_NONCE,
+				    mknonce(turn, nstr, now, src));
+		goto unauth;
+	}
+
+	if (!user || !realm || !nonce) {
+		err = stun_ereply(proto, sock, src, 0, msg,
+				  400, "Bad Request",
+				  NULL, 0, false, 0);
+		goto unauth;
+	}
+
+	if (!nonce_validate(turn, nonce->v.nonce, now, src)) {
+		err = stun_ereply(proto, sock, src, 0, msg,
+				  438, "Stale Nonce",
+				  NULL, 0, false, 2,
+				  STUN_ATTR_REALM, turn->auth_realm,
+				  STUN_ATTR_NONCE,
+				    mknonce(turn, nstr, now, src));
+		goto unauth;
+	}
+
+	ctx->keylen = MD5_SIZE;
+
+	if (auth_get_ha1(turn, user->v.username, ctx->key)) {
+		DEBUG_NOTICE("auth: unknown user '%s' (%j)\n",
+			     user->v.username, src);
+		err = stun_ereply(proto, sock, src, 0, msg,
+				  401, "Unauthorized",
+				  NULL, 0, false, 2,
+				  STUN_ATTR_REALM, turn->auth_realm,
+				  STUN_ATTR_NONCE,
+				    mknonce(turn, nstr, now, src));
+		goto unauth;
+	}
+
+	if (stun_msg_chk_mi(msg, ctx->key, ctx->keylen)) {
+		DEBUG_NOTICE("auth: bad password for user '%s' (%j)\n",
+			     user->v.username, src);
+		err = stun_ereply(proto, sock, src, 0, msg,
+				  401, "Unauthorized",
+				  NULL, 0, false, 2,
+				  STUN_ATTR_REALM, turn->auth_realm,
+				  STUN_ATTR_NONCE,
+				    mknonce(turn, nstr, now, src));
+		goto unauth;
+	}
+
+	return false;
+
+ unauth:
+	if (err) {
+		DEBUG_WARNING("auth reply error: %m\n", err);
+	}
+
+	return true;
+}
+
+
 static void process_msg(struct turnserver *turn, int proto, void *sock,
 			const struct sa *src, struct mbuf *mb)
 {
+	struct auth_context ctx = {0};
 	struct stun_msg *msg = NULL;
 	struct sa laddr;
 	int err = 0;
@@ -168,6 +329,13 @@ static void process_msg(struct turnserver *turn, int proto, void *sock,
 		  stun_method_name(stun_msg_method(msg)));
 #endif
 
+	if (stun_msg_class(msg) == STUN_CLASS_REQUEST) {
+
+		bool unauth = auth_request(turn, &ctx, proto, sock, src, msg);
+		if (unauth)
+			goto out;
+	}
+
 	switch (stun_msg_method(msg)) {
 
 	case STUN_METHOD_ALLOCATE:
@@ -197,7 +365,7 @@ static void process_msg(struct turnserver *turn, int proto, void *sock,
 		udp_rxbuf_presz_set(turn->us_relay, 4);
 
 		err = stun_reply(proto, sock, src, 0,
-				 msg, NULL, 0, false,
+				 msg, ctx.key, ctx.keylen, false,
 				 2,
 				 STUN_ATTR_XOR_MAPPED_ADDR, src,
 				 STUN_ATTR_XOR_RELAY_ADDR, &turn->relay);
@@ -215,7 +383,7 @@ static void process_msg(struct turnserver *turn, int proto, void *sock,
 
 		/* todo: install permissions and check them */
 		err = stun_reply(proto, sock, src, 0,
-				 msg, NULL, 0, false,
+				 msg, ctx.key, ctx.keylen, false,
 				 0);
 	}
 		break;
@@ -240,7 +408,7 @@ static void process_msg(struct turnserver *turn, int proto, void *sock,
 		++turn->chanc;
 
 		err = stun_reply(proto, sock, src, 0,
-				 msg, NULL, 0, false,
+				 msg, ctx.key, ctx.keylen, false,
 				 0);
 	}
 		break;
@@ -474,6 +642,9 @@ int turnserver_alloc(struct turnserver **turnp, const char *addr)
 	err = tcp_sock_local_get(turn->ts, &turn->laddr_tcp);
 	if (err)
 		goto out;
+
+	turn->auth_realm = "RETEST-REALM";
+	turn->auth_secret = 1234;
 
  out:
 	if (err)
