5c686e6fe1afeb99d40af9e858c34070501a18ef
Alfred E. Heggestad <114750+alfredh@users.noreply.github.com>
2025-09-11 10:05:47 +0200

    test: add testing of conf_get_bool()

--- test/conf.c
+++ test/conf.c
@@ -18,7 +18,12 @@ int test_conf(void)
 		"string_val\trattarei\n"
 		"u32_val       42\n"
 		"i32_val       -23\n"
-		"float_val     1.5\n";
+		"float_val     1.5\n"
+		"bool_val_1    true\n"
+		"bool_val_2    Yes\n"
+		"bool_val_3    1\n"
+		"bool_val_4    false\n"
+		;
 	char str[256];
 	struct conf *conf;
 	struct pl pl;
@@ -47,6 +52,24 @@ int test_conf(void)
 	err = conf_get_float(conf, "float_val", &fl);
 	TEST_ERR(err);
 	TEST_EQUALS(1.5, fl);
+
+	bool val = false;
+
+	err = conf_get_bool(conf, "bool_val_1", &val);
+	TEST_ERR(err);
+	ASSERT_TRUE(val);
+
+	err = conf_get_bool(conf, "bool_val_2", &val);
+	TEST_ERR(err);
+	ASSERT_TRUE(val);
+
+	err = conf_get_bool(conf, "bool_val_3", &val);
+	TEST_ERR(err);
+	ASSERT_TRUE(val);
+
+	err = conf_get_bool(conf, "bool_val_4", &val);
+	TEST_ERR(err);
+	ASSERT_TRUE(!val);
 
 	/* Non-existing parameters */
 	if (0 == conf_get(conf, "rattarei", &pl))
