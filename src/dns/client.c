a5df7fb0386bbb1c14c2fed9f26b551b4f6dd33e
Alfred E. Heggestad <114750+alfredh@users.noreply.github.com>
2025-10-21 15:26:57 +0200

    minor fixes

--- src/dns/client.c
+++ src/dns/client.c
@@ -788,8 +788,8 @@ static bool getaddr_dup(struct le *le, void *arg)
 	}
 
 	if (r1->type == DNS_TYPE_AAAA && r2->type == DNS_TYPE_AAAA) {
-		if (r1->rdata.aaaa.addr == r2->rdata.aaaa.addr)
-			return true;
+		if (0 == memcmp(r1->rdata.aaaa.addr, r2->rdata.aaaa.addr, 16))
+			return false;
 	}
 
 	return false;
