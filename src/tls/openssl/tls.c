167daaccb193a524d39100fefd210ec208376820
Alfred E. Heggestad <114750+alfredh@users.noreply.github.com>
2026-04-23 10:05:59 +0200

    tls: upgrade usage of legacy functions
    Upgrade usage of legacy functions to get common name
    from remote peer's certificate.
    
    https://linux.die.net/man/3/x509_name_get_text_by_nid

--- src/tls/openssl/tls.c
+++ src/tls/openssl/tls.c
@@ -1028,8 +1028,19 @@ int tls_peer_common_name(const struct tls_conn *tc, char *cn, size_t size)
 	if (!cert)
 		return ENOENT;
 
-	n = X509_NAME_get_text_by_NID(X509_get_subject_name(cert),
-				      NID_commonName, cn, (int)size);
+	const X509_NAME *name = X509_get_subject_name(cert);
+
+	int ix = X509_NAME_get_index_by_NID(name, NID_commonName, -1);
+	if (ix != -1) {
+		X509_NAME_ENTRY *entry = X509_NAME_get_entry(name, ix);
+		ASN1_STRING *astr = X509_NAME_ENTRY_get_data(entry);
+
+		str_ncpy(cn, (char *)ASN1_STRING_get0_data(astr), size);
+		n = ASN1_STRING_length(astr);
+	}
+	else {
+		n = -1;
+	}
 
 	X509_free(cert);
 
